eca24d67317ce70f38c59fb38a30295c808e9a45
kangkang520 <lujiankang@outlook.com>
2018-03-27 07:04:56 +0800

    问题查看美化，增加评论。

--- src/IssueProvider.ts
+++ src/IssueProvider.ts
@@ -3,12 +3,21 @@ import * as path from 'path'
 import * as fs from 'fs'
 import * as ghutils from 'ghutils'
 
+export interface ICommentNode {
+	id: number
+	user: string
+	body: string
+	issue: number
+}
+
 export interface IIssueNode {
 	id: number
 	title: string
 	body: string
 	labels: Array<string>
 	number: number
+	user: string
+	opened: boolean
 	type: 'issue'
 }
 
@@ -28,12 +37,20 @@ type DataNode = IIssueNode | IStateNode | ILabelNode
 export class IssueProvider implements vscode.TreeDataProvider<DataNode> {
 
 	private issues = { open: null as any, closed: null as any } as { open: Array<IIssueNode>, closed: Array<IIssueNode> }
+	private comments: Array<ICommentNode> = null as any
 	private _onDidChange: vscode.EventEmitter<DataNode> = new vscode.EventEmitter<DataNode>()
+
+	private commentLoadCB: () => any = null as any
 	readonly onDidChangeTreeData = this._onDidChange.event;
 
 	private githubInfo = { user: '', repo: '' }
 
 	private _ghtoken = ''
+
+	private numberInfo = {
+		issue: 0,
+		comment: 0,
+	}
 
 	constructor(private contex: vscode.ExtensionContext) {
 		let { user, repo } = this.getGithubInfo() || {} as any
@@ -41,6 +58,16 @@ export class IssueProvider implements vscode.TreeDataProvider<DataNode> {
 		this.githubInfo.repo = repo || ''
 		this.loadIssues('open')
 		this.loadIssues('closed')
+		this.loadComments()
+		//加载计数信息
+		try {
+			let numberFile = this.contex.asAbsolutePath('res/number.json')
+			if (fs.existsSync(numberFile)) {
+				let result = JSON.parse(fs.readFileSync(numberFile) + '')
+				this.numberInfo = { issue: result.issue || 0, comment: result.comment || 0 }
+			}
+		} catch (e) {
+		}
 	}
 
 	//是否有GitHub配置
@@ -58,11 +85,12 @@ export class IssueProvider implements vscode.TreeDataProvider<DataNode> {
 		if (!this.hasGithubConfig) return { label: '当前项目非GitHub项目' }
 		if (elem.type == 'issue') return {
 			id: elem.id + '',
-			label: elem.title,
-			iconPath: this.contex.asAbsolutePath('res/' + this.getLabelImage(elem.labels)),
+			label: `[${this.commentsOfIssue(elem.number).length}] ${elem.title}`,
+			// resourceUri: vscode.Uri.parse('before:github-issue-' + this.getLabelImage(elem.labels)),
+			iconPath: this.contex.asAbsolutePath('res/' + this.getLabelImage(elem.labels) + '.svg'),
 			tooltip: elem.title,
 			collapsibleState: vscode.TreeItemCollapsibleState.None,
-			contextValue: 'openedIssue',
+			contextValue: elem.opened ? 'openedIssue' : 'closedIssue',
 			command: {
 				title: "查看Issue",
 				command: "gitissue.viewOnCommand",
@@ -101,10 +129,10 @@ export class IssueProvider implements vscode.TreeDataProvider<DataNode> {
 
 	//获取label对应的图片
 	private getLabelImage(labels: Array<string>) {
-		if (labels.indexOf('bug') >= 0) return 'bug.svg'
-		if (labels.indexOf('question') >= 0) return 'question.svg'
-		if (labels.indexOf('help wanted') >= 0) return 'require.svg'
-		return 'other.svg'
+		if (labels.indexOf('bug') >= 0) return 'bug'
+		if (labels.indexOf('question') >= 0) return 'question'
+		if (labels.indexOf('help wanted') >= 0) return 'require'
+		return 'other'
 	}
 
 	//获取GitHub token
@@ -141,39 +169,68 @@ export class IssueProvider implements vscode.TreeDataProvider<DataNode> {
 	//加载问题列表
 	public async loadIssues(state: 'open' | 'closed') {
 		if (!this.hasGithubConfig) return
+		let token = await this.autoCreateToken()
 		this.issues[state] = null as any
 		this.refresh()
-		ghutils.lister({ token: '' }, `https://api.github.com/repos/${this.githubInfo.user}/${this.githubInfo.repo}/issues?state=${state}`, {}, (err: Error, result: Array<IIssueNode>) => {
+		ghutils.lister({ token }, `https://api.github.com/repos/${this.githubInfo.user}/${this.githubInfo.repo}/issues?state=${state}`, {}, (err: Error, result: Array<IIssueNode>) => {
 			if (err) return vscode.window.showErrorMessage(err.message)
-			this.issues[state] = result.map(i => ({
+			this.issues[state] = result.map((i: IIssueNode & { [i: string]: any, user: any }) => ({
 				id: i.id,
 				title: i.title,
 				body: i.body,
 				labels: i.labels.map((l: any) => l.name),
 				number: i.number,
-				type: 'issue'
-			} as IIssueNode))
+				type: 'issue' as any,
+				opened: state == 'open',
+				user: i.user.login
+			}))
 			this.refresh()
 			if (state == 'open') {
-				let toast = false
 				let maxNumber = Math.max(...result.map(r => r.number))
-				let numberFile = this.contex.asAbsolutePath('res/number.json')
-				try {
-					if (!fs.existsSync(numberFile)) toast = true
-					else if (parseInt(fs.readFileSync(numberFile) + '') < maxNumber) toast = true
-				} catch (e) {
-				}
-				if (toast) {
+				if (maxNumber > this.numberInfo.issue) {
 					let issue = result.filter(r => r.number == maxNumber)[0]
-					vscode.window.showInformationMessage('新问题：' + issue.body)
-					this.setNumber(maxNumber)
+					vscode.window.showInformationMessage('GitHub新问题：' + issue.body)
+					this.setNumber(maxNumber, this.numberInfo.comment)
 				}
 			}
 		})
 	}
 
-	private setNumber(nu: number) {
-		fs.writeFileSync(this.contex.asAbsolutePath('res/number.json'), nu)
+	//加载评论
+	public async loadComments() {
+		if (!this.hasGithubConfig) return
+		let token = await this.autoCreateToken()
+		this.comments = null as any
+		this.refresh()
+		ghutils.lister({ token }, `https://api.github.com/repos/${this.githubInfo.user}/${this.githubInfo.repo}/issues/comments`, {}, (err: Error, result: Array<any>) => {
+			if (err) return vscode.window.showErrorMessage(err.message)
+			this.comments = result.map(comment => ({
+				id: comment.id,
+				user: comment.user.login,
+				body: comment.body,
+				issue: this.getIssueNumberFromIssueUrl(comment.issue_url)
+			}))
+			this.commentLoadCB && this.commentLoadCB()
+			this.refresh()
+			//提示
+			let maxId = Math.max(...result.map(r => r.id))
+			if (maxId > this.numberInfo.comment) {
+				let comment = result.filter(r => r.id == maxId)[0]
+				vscode.window.showInformationMessage('GitHub新评论：\n' + comment.body)
+				this.setNumber(this.numberInfo.issue, maxId)
+			}
+		})
+	}
+
+	//获取某个Issue的评论
+	public commentsOfIssue(issueNumber: number) {
+		return this.comments.filter(c => c.issue == issueNumber)
+	}
+
+	//保存计数
+	private setNumber(issue: number, comment: number) {
+		this.numberInfo = { issue, comment }
+		fs.writeFileSync(this.contex.asAbsolutePath('res/number.json'), JSON.stringify(this.numberInfo))
 	}
 
 	private labelName(label: string): string | undefined {
@@ -182,6 +239,11 @@ export class IssueProvider implements vscode.TreeDataProvider<DataNode> {
 			'需求': 'help wanted',
 			'问题': 'question',
 		} as any)[label] || undefined
+	}
+
+	private getIssueNumberFromIssueUrl(url: string): number {
+		let [, id] = url.match(/\/(\d+)$/) || [] as any
+		return parseInt(id)
 	}
 
 	//添加问题
@@ -207,13 +269,35 @@ export class IssueProvider implements vscode.TreeDataProvider<DataNode> {
 		label = this.labelName(label)
 		ghutils.ghpost({ token }, `https://api.github.com/repos/${this.githubInfo.user}/${this.githubInfo.repo}/issues`, { title, body, ...(label ? { labels: [label] } : {}) }, {}, (err: Error, issue: IIssueNode) => {
 			if (err) return vscode.window.showErrorMessage(err.message)
-			setTimeout(() => this.loadIssues('open'), 1000)
-			this.setNumber(issue.number)
+			setTimeout(() => this.loadIssues('open'), 1500)
+			this.setNumber(issue.number, this.numberInfo.comment)
 		})
 	}
 
 	public getIssueUrl(number: number): string {
 		return `https://github.com/${this.githubInfo.user}/${this.githubInfo.repo}/issues/${number}`
+	}
+
+	//评论加载完毕事件
+	public onCommentLoad(callback: () => any) {
+		this.commentLoadCB = callback
+	}
+
+	//发起评论
+	public async createComment(issue: IIssueNode) {
+		let token = await this.autoCreateToken()
+		if (!this.hasGithubConfig || !token) return
+		let body = await vscode.window.showInputBox({
+			prompt: '请输入评论内容，至少5个字',
+			placeHolder: '评论内容',
+			validateInput: str => (str && str.length < 5) ? '至少输入5个字' : null
+		})
+		if (!body) return
+		ghutils.ghpost({ token }, `https://api.github.com/repos/${this.githubInfo.user}/${this.githubInfo.repo}/issues/${issue.number}/comments`, { body }, (err: Error, comment: any) => {
+			if (err) return vscode.window.showErrorMessage(err.message)
+			setTimeout(() => this.loadComments(), 1500)
+			this.setNumber(this.numberInfo.issue, comment.id)
+		})
 	}
 
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,49 @@
 import * as vscode from 'vscode'
 import { IssueProvider, IIssueNode } from './IssueProvider'
 
+
 export function activate(context: vscode.ExtensionContext) {
+	let currentViewIssue: IIssueNode = null as any
 
 	let issueProvider = new IssueProvider(context)
 	context.subscriptions.push(vscode.window.registerTreeDataProvider('githubIssueBrowser', issueProvider))
 
 	const output = vscode.window.createOutputChannel('GitHub问题输出')
+	function showIssueDetails(issue: IIssueNode) {
+		currentViewIssue = issue
+		output.show()
+		output.clear()
+		output.appendLine(`问题:${issue.title}`)
+		output.appendLine(`来源:[@${issue.user}]`)
+		output.appendLine(`内容:\n${issue.body}`)
+		output.append('\n')
+		//获取回复列表
+		issueProvider.commentsOfIssue(issue.number).forEach(comment => {
+			output.appendLine(`评论:[@${comment.user}]\n${comment.body}\n`)
+		})
+	}
 
+
+	issueProvider.onCommentLoad(() => {
+		if (!currentViewIssue) return
+		showIssueDetails(currentViewIssue)
+	})
 	context.subscriptions.push(vscode.commands.registerCommand("gitissue.addIssue", () => {
 		issueProvider.createIssue()
 	}));
 	context.subscriptions.push(vscode.commands.registerCommand("gitissue.viewOnCommand", async (node: IIssueNode) => {
-		output.show()
-		output.clear()
-		output.append(node.title)
-		output.append('\n-----------------\n')
-		output.append(node.body)
+		showIssueDetails(node)
 	}));
 	context.subscriptions.push(vscode.commands.registerCommand("gitissue.refreshIssues", () => {
 		issueProvider.loadIssues('open')
 		issueProvider.loadIssues('closed')
+		issueProvider.loadComments()
 	}));
 	context.subscriptions.push(vscode.commands.registerCommand("gitissue.setGithubToken", () => {
 		issueProvider.autoCreateToken(true)
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand("gitissue.addComment", (node: IIssueNode) => {
+		issueProvider.createComment(node)
 	}));
 }
 
